fcb71418e96a1f9b47b281a99f69e976b5d1904d
Elvis <steevenbailon23@gmail.com>
2025-05-11 15:59:14 -0500

    se logro completar el diseño del login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Login/login/login.component';
+import { registerDispatcher } from '@angular/core/primitives/event-dispatch';
+import { RegistroComponent } from './Login/registro/registro.component';
 
 export const routes: Routes = [
-    {path: "login", component: LoginComponent}
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegistroComponent}
 ];
